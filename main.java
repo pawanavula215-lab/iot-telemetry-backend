2a04a2f5cdaf90aba9923bc49c40d6f92777f1cb
Maruthi Pawan Avula <pawanavula215@gmail.com>
2025-10-30 12:06:10 -0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,6 @@
+// IoT Telemetry Backend - Placeholder code
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("IoT Telemetry Backend API - Java Spring Boot coming soon");
+    }
+}
